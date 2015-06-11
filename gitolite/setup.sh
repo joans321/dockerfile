7c7fd6ae12211e3ecd5a6b97c8b1538492c7eb08
xuejianqing <xuejianqing@star-net.cn>
2015-06-11 09:46:26 +0800

    First base images

--- gitolite/setup.sh
+++ gitolite/setup.sh
@@ -1,19 +1,27 @@
 #!/bin/sh
 
+export PATH=$PATH:/usr/bin/gitolite
+GITOLITE=/usr/local/gitolite/gitolite
+
 GITHOME=/home/git
 INITFILE=$GITHOME/.gitserver_init
 
 if [ ! -f $INITFILE ]; then
     cd $GITHOME
+    echo $PATH
+    echo '---------------'
     if [ ! -d .gitolite ]; then
-        su git -c "gitolite setup -a dummy"
+        su git -c "$GITOLITE setup -a dummy"
     fi
     touch $INITFILE
 fi
 
 service ssh start
 
-while [ 1 ]; do
-  echo "Dummy echo"
-  sleep 60
-done
+if [ $# -ne 0 ]; then
+    exec $*
+else
+    while [ 1 ]; do sleep 60;  done
+fi
+
+
