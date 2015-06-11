3c40f31f9836d60ce46ff403354c01feed3db24f
xuejianqing <xuejianqing@star-net.cn>
2015-06-11 13:12:50 +0800

    update readme, support setup and import gitolite

--- gitolite/README.md
+++ gitolite/README.md
@@ -1,13 +1,36 @@
 # Gitolite
 
-Gitolite image base on ubuntu 14.04 and support auto send after repo update.
+Gitolite image base on ubuntu 14.04 and support auto send email after repo update.
 
 # Feature
 
 * gitolite server
-* support jenkins
-* support auto mail
-* support import old repositories
+* support import repositories
+* support auto email when update repo --- pending
+* support jenkins notify ---- pending
+
+# Usage
+
+## New Git Server
+Gitolite only need your ssh public key if you are first time to use it.
+Generator a new ssh key in the *~/.ssh* directory with command :
+
+    $ ssh-keygen -f ~/.ssh/id_rsa  -t rsa -N ''
+
+If you have any issue, refer to https://help.github.com/articles/generating-ssh-keys
+
+Run docker command to start gitolite server with your ssh public key :
+
+    $ sudo docker run -it --rm -e SSH_KEY="$(cat ~/.ssh/id_rsa.pub)" joans321/gitolite
+
+## Import Git Repositories
+
+If you already have repositories, what you need to do is import them.
+
+Run docker command to start gitolite with your repositories :
+
+	$ sudo docker run -it --rm -v YourRepoDir:/home/git/repositories joans321/gitolite
+
 
 # Require
 * admin's pub key for new repositories
--- gitolite/setup.sh
+++ gitolite/setup.sh
@@ -1,21 +1,54 @@
-#!/bin/sh
+#!/bin/bash
 
 export PATH=$PATH:/usr/bin/gitolite
 export LANG="en_US.UTF-8"
 export LANGUAGE="en_US:en"
-
 
 GITOLITE=/usr/local/gitolite/gitolite
 
 GITHOME=/home/git
 INITFILE=$GITHOME/.gitserver_init
 
+function setup_gitolite() {
+    if [ -n "$SSH_KEY" ]; then
+	echo "create repositories with your ssh public key !"
+	echo "$SSH_KEY" > /tmp/admin.pub
+	su git -c "$GITOLITE setup -pk /tmp/admin.pub"
+	rm /tmp/admin.pub
+    else
+	su git -c "$GITOLITE setup -a dummy"
+    fi
+}
+
+function import_repo() {
+    GITADMIN_TMPD=/tmp/gitolite-admin.git
+
+    chown -R git.git  repositories
+
+    if [ -d ./repositories/gitolite-admin.git ]; then
+	mv ./repositories/gitolite-admin.git  $GITADMIN_TMPD
+    fi
+
+    setup_gitolite
+
+    if [ -d $GITADMIN_TMPD ]; then
+	rm -rf ./repositories/gitolite-admin.git
+	mv $GITADMIN_TMPD ./repositories/gitolite-admin.git
+
+	# update authorized_keys
+	su git -c "cd /home/git/repositories/gitolite-admin.git && GL_LIBDIR=$($GITOLITE query-rc GL_LIBDIR) PATH=$PATH hooks/post-update refs/heads/master"
+    fi
+}
+
 if [ ! -f $INITFILE ]; then
     cd $GITHOME
-    echo $PATH
-    echo '---------------'
+
+    if [ -d repositories ]; then
+	import_repo
+    fi
+
     if [ ! -d .gitolite ]; then
-        su git -c "$GITOLITE setup -a dummy"
+        setup_gitolite
     fi
     touch $INITFILE
 fi
