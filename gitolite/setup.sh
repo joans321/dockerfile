3e58ef1872bc274e948827246e22179b488f6779
xuejianqing <xuejianqing@star-net.cn>
2015-06-11 09:27:21 +0800

    fix su command

--- gitolite/setup.sh
+++ gitolite/setup.sh
@@ -6,7 +6,7 @@ INITFILE=$GITHOME/.gitserver_init
 if [ ! -f $INITFILE ]; then
     cd $GITHOME
     if [ ! -d .gitolite ]; then
-        su git -c gitolite setup -a dummy
+        su git -c "gitolite setup -a dummy"
     fi
     touch $INITFILE
 fi
